7da1d2fc960c73431c8a34d8e0c95e0c52b60492
mauro <figueroamauro.dev@gmail.com>
2025-03-15 22:17:41 -0300

    feat(UsuarioController): se agregó el método para buscar Usuario por id.

--- backend/src/main/java/idforideas/bonpland/controller/UsuarioController.java
+++ backend/src/main/java/idforideas/bonpland/controller/UsuarioController.java
@@ -2,16 +2,16 @@
 package idforideas.bonpland.controller;
 
 import idforideas.bonpland.dto.usuarios.UsuarioRespuestaDTO;
+import idforideas.bonpland.entities.Usuario;
 import idforideas.bonpland.mapper.impl.UsuarioRespuestaMapper;
 import idforideas.bonpland.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Figueroa Mauro
@@ -33,5 +33,11 @@ public class UsuarioController {
                                             PagedResourcesAssembler<UsuarioRespuestaDTO> pagedResourcesAssembler) {
         Page<UsuarioRespuestaDTO> usuarios = usuarioService.listarUsuarios(pageable).map(mapper::map);
         return ResponseEntity.ok(pagedResourcesAssembler.toModel(usuarios));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioRespuestaDTO> buscarUsuarioPorId(@PathVariable Long id) {
+        Usuario usuario = usuarioService.buscarUsuarioPorId(id);
+        return ResponseEntity.ok(mapper.map(usuario));
     }
 }
--- backend/src/test/java/idforideas/bonpland/controller/UsuarioControllerTest.java
+++ backend/src/test/java/idforideas/bonpland/controller/UsuarioControllerTest.java
@@ -1,14 +1,17 @@
 package idforideas.bonpland.controller;
 
+import idforideas.bonpland.dto.usuarios.UsuarioCompletoDTO;
 import idforideas.bonpland.entities.Usuario;
 import idforideas.bonpland.mapper.impl.UsuarioRespuestaMapper;
 import idforideas.bonpland.service.UsuarioService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static idforideas.bonpland.utils.TestUtil.getUsuario;
 import static org.eclipse.persistence.expressions.ExpressionOperator.exists;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -38,7 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UsuarioControllerTest {
     public static final String PATH_USUARIOS = "/api/usuarios";
 
-    private List<Usuario>lista;
+    private List<Usuario> lista;
     private Pageable pageable;
     @Autowired
     private MockMvc mockMvc;
@@ -48,7 +51,7 @@ class UsuarioControllerTest {
     @BeforeEach
     void init() {
         lista = new ArrayList<>();
-        pageable =  PageRequest.of(0,10);
+        pageable = PageRequest.of(0, 10);
     }
 
 
@@ -56,12 +59,11 @@ class UsuarioControllerTest {
     void deberiaListarUsuariosYRetornar200() throws Exception {
         //GIVEN
         lista = getUsuarios();
-
         when(usuarioService.listarUsuarios(pageable)).thenReturn(new PageImpl<>(lista, pageable, lista.size()));
 
         //WHEN
         mockMvc.perform(get(PATH_USUARIOS)
-                .contentType(MediaType.APPLICATION_JSON))
+                                .contentType(MediaType.APPLICATION_JSON))
 
                 //THEN
                 .andExpect(status().isOk())
@@ -72,6 +74,8 @@ class UsuarioControllerTest {
                 .andExpect(jsonPath("$._embedded.usuarioRespuestaDTOList[0].nombre").value(lista.get(0).getNombre()))
                 .andExpect(jsonPath("$._links.self").exists())
                 .andExpect(jsonPath("$.page.totalElements").value(lista.size()));
+
+        verify(usuarioService).listarUsuarios(any(Pageable.class));
     }
 
     @Test
@@ -83,8 +87,27 @@ class UsuarioControllerTest {
         mockMvc.perform(get(PATH_USUARIOS)
                                 .contentType(MediaType.APPLICATION_JSON))
 
-        //THEN
+                //THEN
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$._embedded").doesNotExist());
+
+        verify(usuarioService).listarUsuarios(any(Pageable.class));
+    }
+
+    @Test
+    void deberiaRetornarUsuarioPorIdY200_cuandoExiste() throws Exception {
+        //GIVEN
+        when(usuarioService.buscarUsuarioPorId(1L)).thenReturn(getUsuario());
+
+        //WHEN
+        mockMvc.perform(get(PATH_USUARIOS + "/1")
+                                .contentType(MediaType.APPLICATION_JSON))
+
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                 .andExpect(jsonPath("$.nombre").value("test"))
+                .andExpect(jsonPath("$.apellido").value("test"))
+                .andExpect(jsonPath("$.correo").value("test@mail.com"));
     }
 }
