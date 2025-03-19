5c0493aa9888d3f800d9dca7ac26387491d1001e
mauro <figueroamauro.dev@gmail.com>
2025-03-19 13:31:10 -0300

    test(AuthControllerTest): se agregó test para validar la autenticación con credenciales correctas

--- backend/src/test/java/idforideas/bonpland/controller/AuthControllerTest.java
+++ backend/src/test/java/idforideas/bonpland/controller/AuthControllerTest.java
@@ -1,14 +1,18 @@
 package idforideas.bonpland.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import idforideas.bonpland.config.SecurityConfig;
 import idforideas.bonpland.dto.DTO;
+import idforideas.bonpland.dto.auth.LoginDTO;
 import idforideas.bonpland.dto.usuarios.UsuarioCompletoDTO;
 import idforideas.bonpland.exception.CorreoExistenteException;
 import idforideas.bonpland.mapper.impl.UsuarioRespuestaMapper;
 import idforideas.bonpland.security.CustomUserDetailService;
+import idforideas.bonpland.security.CustomUserDetails;
 import idforideas.bonpland.security.JwtService;
 import idforideas.bonpland.service.UsuarioService;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static idforideas.bonpland.utils.TestUtil.getUsuario;
@@ -21,6 +25,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -32,7 +39,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- *
  * @author Figueroa Mauro
  */
 @WebMvcTest(controllers = AuthController.class)
@@ -40,6 +46,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Import({UsuarioRespuestaMapper.class, SecurityConfig.class})
 class AuthControllerTest {
     public static final String PATH_REGISTRO = "/api/auth/registro";
+    public static final String PATH_LOGIN = "/api/auth/login";
     @Autowired
     private MockMvc mockMvc;
     @MockitoBean
@@ -51,85 +58,118 @@ class AuthControllerTest {
     @MockitoBean
     private JwtService jwtService;
 
-    @Test
-    void deberiaCrearUsuarioYRetornar201() throws Exception {
+    @MockitoBean
+    private AuthenticationManager authenticationManager;
+    @MockitoBean
+    Authentication authMock;
 
-        //GIVEN
-        when(usuarioService.guardarUsuario(getUsuarioDTO())).thenReturn(getUsuario());
 
-        //WHEN
-        mockMvc.perform(post(PATH_REGISTRO)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(getUsuarioDTO())))
+    @Nested
+    class RegistroTest {
 
-                //THEN
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.nombre").value("test"))
-                .andExpect(jsonPath("$.apellido").value("test"))
-                .andExpect(jsonPath("$.correo").value("test@mail.com"));
+        @Test
+        void deberiaCrearUsuarioYRetornar201() throws Exception {
 
-        verify(usuarioService, times(1)).guardarUsuario(any(UsuarioCompletoDTO.class));
+            //GIVEN
+            when(usuarioService.guardarUsuario(getUsuarioDTO())).thenReturn(getUsuario());
+
+            //WHEN
+            mockMvc.perform(post(PATH_REGISTRO)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(getUsuarioDTO())))
+
+                    //THEN
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.id").exists())
+                    .andExpect(jsonPath("$.nombre").value("test"))
+                    .andExpect(jsonPath("$.apellido").value("test"))
+                    .andExpect(jsonPath("$.correo").value("test@mail.com"));
+
+            verify(usuarioService, times(1)).guardarUsuario(any(UsuarioCompletoDTO.class));
+        }
+
+        @Test
+        void deberiaRetornar400YListaDeErrores_cuandoDTOEsInvalido() throws Exception {
+            //GIVEN
+            DTO dto = new UsuarioCompletoDTO(null, "", "test@", "123123", "correo@mail", "1122334455");
+
+            //WHEN
+            mockMvc.perform(post(PATH_REGISTRO)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(dto)))
+
+                    //THEN
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").isArray())
+                    .andExpect(jsonPath("$.error", hasItems("El nombre debe tener entre 2 y 50 caracteres",
+                            "Formato de correo inválido", "El apellido no puede contener caracteres especiales",
+                            "El nombre no puede contener caracteres especiales",
+                            "La clave debe tener entre 8 y 20 caracteres")));
+
+            verify(usuarioService, never()).guardarUsuario(any(UsuarioCompletoDTO.class));
+        }
+
+        @Test
+        void deberiaRetornar400YUnError_cuandoDTOTieneUnCampoInvalido() throws Exception {
+            //GIVEN
+            UsuarioCompletoDTO dto = getUsuarioDTO();
+            dto.setNombre("Error400");
+
+            //WHEN
+            mockMvc.perform(post(PATH_REGISTRO)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(dto)))
+
+                    //THEN
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").isString())
+                    .andExpect(jsonPath("$.error").value("El nombre no puede contener caracteres especiales"));
+
+            verify(usuarioService, never()).guardarUsuario(any(UsuarioCompletoDTO.class));
+        }
+
+        @Test
+        void deberiaRetornar400_cuandoDTOTieneCorreoExistente() throws Exception {
+            //GIVEN
+            UsuarioCompletoDTO dto = getUsuarioDTO();
+            when(usuarioService.guardarUsuario(dto)).thenThrow(new CorreoExistenteException("El correo ya existe"));
+
+            //WHEN
+            mockMvc.perform(post(PATH_REGISTRO)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(dto)))
+
+                    //THEN
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.error").isString())
+                    .andExpect(jsonPath("$.error").value("El correo ya existe"));
+
+            verify(usuarioService).guardarUsuario(any(UsuarioCompletoDTO.class));
+        }
+
     }
 
-    @Test
-    void deberiaRetornar400YListaDeErrores_cuandoDTOEsInvalido() throws Exception {
-        //GIVEN
-        DTO dto = new UsuarioCompletoDTO(null, "", "test@", "123123", "correo@mail", "1122334455");
+    @Nested
+    class LoginTest {
 
-        //WHEN
-        mockMvc.perform(post(PATH_REGISTRO)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(dto)))
+        @Test
+        void deberiaAutenticarseConCredencialesValidas() throws Exception {
+            //GIVEN
+            LoginDTO loginRequest = new LoginDTO("test@mail.com", "clave123");
 
-                //THEN
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").isArray())
-                .andExpect(jsonPath("$.error", hasItems("El nombre debe tener entre 2 y 50 caracteres",
-                        "Formato de correo inválido","El apellido no puede contener caracteres especiales",
-                        "El nombre no puede contener caracteres especiales",
-                        "La clave debe tener entre 8 y 20 caracteres")));
+            when(authMock.getPrincipal()).thenReturn(new CustomUserDetails(getUsuario()));
+            when(authenticationManager.authenticate(any(Authentication.class))).thenReturn(authMock);
+            when(jwtService.generarToken(any(CustomUserDetails.class))).thenReturn("fakeToken123");
 
-        verify(usuarioService,never()).guardarUsuario(any(UsuarioCompletoDTO.class));
+            //WHEN
+            mockMvc.perform(post(PATH_LOGIN).contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(loginRequest)))
+
+            //THEN
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").isMap())
+                    .andExpect(jsonPath("$.Token").value("fakeToken123"));
+        }
     }
-
-    @Test
-    void deberiaRetornar400YUnError_cuandoDTOTieneUnCampoInvalido() throws Exception {
-        //GIVEN
-        UsuarioCompletoDTO dto = getUsuarioDTO();
-        dto.setNombre("Error400");
-
-        //WHEN
-        mockMvc.perform(post(PATH_REGISTRO)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(dto)))
-
-                //THEN
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").isString())
-                .andExpect(jsonPath("$.error").value("El nombre no puede contener caracteres especiales"));
-
-        verify(usuarioService,never()).guardarUsuario(any(UsuarioCompletoDTO.class));
-    }
-
-    @Test
-    void deberiaRetornar400_cuandoDTOTieneCorreoExistente() throws Exception {
-        //GIVEN
-        UsuarioCompletoDTO dto = getUsuarioDTO();
-        when(usuarioService.guardarUsuario(dto)).thenThrow(new CorreoExistenteException("El correo ya existe"));
-
-        //WHEN
-        mockMvc.perform(post(PATH_REGISTRO)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(dto)))
-
-                //THEN
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").isString())
-                .andExpect(jsonPath("$.error").value("El correo ya existe"));
-
-        verify(usuarioService).guardarUsuario(any(UsuarioCompletoDTO.class));
-    }
-
 
 }
