01bc47ed86e2f27c5c024cb12955ba6b35e0be2f
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-03-22 18:08:51 -0300

    test(CustomUserDetailServiceTest): se agregó test para validar que lance exception cuando no se encuentra el usuario por su correo. -throw UsuarioNoEncontradoException

--- backend/src/test/java/idforideas/bonpland/security/CustomUserDetailServiceTest.java
+++ backend/src/test/java/idforideas/bonpland/security/CustomUserDetailServiceTest.java
@@ -1,11 +1,14 @@
 package idforideas.bonpland.security;
 
 import idforideas.bonpland.entities.Usuario;
+import idforideas.bonpland.exception.UsuarioNoEncontradoException;
 import idforideas.bonpland.repository.UsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -25,19 +28,34 @@ class CustomUserDetailServiceTest {
     CustomUserDetailService userDetailService;
 
     @Test
-    void deberiaBuscarUsuarioPorSuCorreo(){
+    void deberiaBuscarUsuarioPorSuCorreo() {
         //GIVEN
         String correo = "correo@test.com";
         Usuario usuario = getUsuario();
         usuario.setCorreo(correo);
         when(usuarioRepository.findByCorreo(correo)).thenReturn(Optional.of(usuario));
-    
+
         //WHEN
         UserDetails userDetails = userDetailService.loadUserByUsername(correo);
 
         //THEN
         assertNotNull(userDetails);
-        assertEquals(correo,userDetails.getUsername());
+        assertEquals(correo, userDetails.getUsername());
+
+        verify(usuarioRepository).findByCorreo(correo);
     }
 
+    @Test
+    void deberiaLanzarExcepcion_cuandoElUsuarioNoSeEncuentra() {
+        //GIVEN
+        String correo = "correo@test.com";
+        when(usuarioRepository.findByCorreo(correo)).thenReturn(Optional.empty());
+
+        //WHEN
+        Executable executable = () -> userDetailService.loadUserByUsername(correo);
+
+        //THEN
+        UsuarioNoEncontradoException e = assertThrows(UsuarioNoEncontradoException.class, executable);
+        assertEquals("Usuario no encontrado", e.getMessage());
+    }
 }
