22854786c93d183f041aeb1e7f763537fbec672d
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-03-25 15:40:00 -0300

    feat(InmuebleServiceImple): se agregó validación para que el inmueble sea creado por el usuario logueado en el sistema.

--- backend/src/main/java/idforideas/bonpland/service/impl/InmuebleServiceImpl.java
+++ backend/src/main/java/idforideas/bonpland/service/impl/InmuebleServiceImpl.java
@@ -2,12 +2,18 @@ package idforideas.bonpland.service.impl;
 
 import idforideas.bonpland.dto.inmuebles.InmuebleDTO;
 import idforideas.bonpland.entities.Inmueble;
+import idforideas.bonpland.entities.Usuario;
 import idforideas.bonpland.exception.InmuebleNoEncontradoException;
+import idforideas.bonpland.exception.UsuarioNoEncontradoException;
 import idforideas.bonpland.mapper.impl.InmuebleMapper;
 import idforideas.bonpland.repository.InmuebleRepository;
+import idforideas.bonpland.repository.UsuarioRepository;
+import idforideas.bonpland.security.CustomUserDetailService;
 import idforideas.bonpland.service.InmuebleService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,18 +24,25 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class InmuebleServiceImpl implements InmuebleService {
-    private final InmuebleRepository repository;
+    private final InmuebleRepository inmuebleRepository;
+    private final UsuarioRepository usuarioRepository;
 
     private final InmuebleMapper mapper;
+    private CustomUserDetailService customUserDetailService;
 
     @Override
     public Inmueble guardarInmueble(InmuebleDTO dto) {
+        String correo = SecurityContextHolder.getContext().getAuthentication().getName();
+        Usuario usuario = usuarioRepository.findByCorreo(correo)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
         if (dto == null) {
             throw new IllegalArgumentException("Inmueble no puede ser nulo");
         }
         Inmueble inmueble = mapper.map(dto);
         inmueble.setId(null);
-        return repository.save(inmueble);
+        inmueble.setUsuario(usuario);
+        return inmuebleRepository.save(inmueble);
     }
 
     @Override
@@ -39,9 +52,10 @@ public class InmuebleServiceImpl implements InmuebleService {
 
     @Override
     public Inmueble buscarInmueblePorId(Long id) {
-        Optional<Inmueble> inmueble = repository.findById(id);
+        Optional<Inmueble> inmueble = inmuebleRepository.findById(id);
         return inmueble.orElseThrow(()-> new InmuebleNoEncontradoException("Inmueble no encontrado"));
     }
+
     @Override
     public Inmueble actualizarInmueble(InmuebleDTO dto) {
         return null;
--- backend/src/test/java/idforideas/bonpland/service/InmuebleServiceImplTest.java
+++ backend/src/test/java/idforideas/bonpland/service/InmuebleServiceImplTest.java
@@ -2,14 +2,19 @@ package idforideas.bonpland.service;
 
 import idforideas.bonpland.dto.inmuebles.InmuebleDTO;
 import idforideas.bonpland.entities.Inmueble;
+import idforideas.bonpland.entities.Usuario;
 import idforideas.bonpland.enumerations.TipoPropiedad;
 import idforideas.bonpland.exception.InmuebleNoEncontradoException;
 import idforideas.bonpland.mapper.impl.InmuebleMapper;
 import idforideas.bonpland.repository.InmuebleRepository;
+import idforideas.bonpland.repository.UsuarioRepository;
 import idforideas.bonpland.service.impl.InmuebleServiceImpl;
 
+import static idforideas.bonpland.utils.TestUtil.getUsuario;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
@@ -21,7 +26,12 @@ import static org.mockito.Mockito.*;
 
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 /**
@@ -29,20 +39,30 @@ import java.util.Optional;
  */
 @ExtendWith(MockitoExtension.class)
 class InmuebleServiceImplTest {
-
+    private InmuebleDTO dto;
+    private Usuario usuario;
     @Mock
     private InmuebleRepository inmuebleRepository;
-
     @InjectMocks
-    InmuebleServiceImpl inmuebleService;
+    private InmuebleServiceImpl inmuebleService;
     @Spy
-    InmuebleMapper mapper;
+    private InmuebleMapper mapper;
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    void init() {
+        dto = getDto();
+        usuario = getUsuario();
+        Authentication auth = new UsernamePasswordAuthenticationToken(usuario.getCorreo(), null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
 
     @Test
-    void deberiaGuardarInmueble(){
+    @WithMockUser(username = "test", roles = "USUARIO")
+    void deberiaGuardarInmueble_unicamenteUsuarioLogueado() {
         //GIVEN
-        InmuebleDTO dto = new InmuebleDTO(1L, "descripcion", 1000, "Direccion", 1999,
-                3, 4, 1, 1, 100, TipoPropiedad.CASA);
+        when(usuarioRepository.findByCorreo(usuario.getCorreo())).thenReturn(Optional.of(usuario));
 
         //WHEN
         inmuebleService.guardarInmueble(dto);
@@ -51,8 +71,12 @@ class InmuebleServiceImplTest {
         verify(inmuebleRepository).save(any(Inmueble.class));
     }
 
+
     @Test
     void deberiaLanzarException_cuandoLosDatosSonNulos() {
+        //GIVEN
+        when(usuarioRepository.findByCorreo(usuario.getCorreo())).thenReturn(Optional.of(usuario));
+
         //WHEN
         Executable executable = () -> inmuebleService.guardarInmueble(null);
 
@@ -60,14 +84,13 @@ class InmuebleServiceImplTest {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, executable);
         assertEquals("Inmueble no puede ser nulo", e.getMessage());
 
-        verify(inmuebleRepository,never()).save(any());
+        verify(inmuebleRepository, never()).save(any());
     }
 
     @Test
     void deberiaSetearElIdEnNuloAntesDeGuardar() {
         //GIVEN
-        InmuebleDTO dto = new InmuebleDTO(1L, "descripcion", 1000, "Direccion", 1999,
-                3, 4, 1, 1, 100, TipoPropiedad.CASA);
+        when(usuarioRepository.findByCorreo(usuario.getCorreo())).thenReturn(Optional.of(usuario));
 
         //WHEN
         inmuebleService.guardarInmueble(dto);
@@ -83,7 +106,7 @@ class InmuebleServiceImplTest {
     }
 
     @Test
-    void deberiaBuscarPorId(){
+    void deberiaBuscarPorId() {
         //GIVEN
         Long id = 1L;
         when(inmuebleRepository.findById(id)).thenReturn(Optional.of(new Inmueble()));
@@ -98,7 +121,7 @@ class InmuebleServiceImplTest {
     }
 
     @Test
-    void deberiaLanzarException_cuandoNoEncuentraElInmueble(){
+    void deberiaLanzarException_cuandoNoEncuentraElInmueble() {
         //GIVEN
         Long id = 1L;
         when(inmuebleRepository.findById(id)).thenReturn(Optional.empty());
@@ -109,6 +132,13 @@ class InmuebleServiceImplTest {
         //THEN
         InmuebleNoEncontradoException e = assertThrows(InmuebleNoEncontradoException.class, executable);
         assertEquals("Inmueble no encontrado", e.getMessage());
+
+        verify(inmuebleRepository, never()).save(any());
+    }
+
+    private static @NotNull InmuebleDTO getDto() {
+        return new InmuebleDTO(1L, "descripcion", 1000, "Direccion", 1999,
+                3, 4, 1, 1, 100, TipoPropiedad.CASA);
     }
 
 }
