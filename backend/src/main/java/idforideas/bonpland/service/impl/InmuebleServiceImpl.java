78e2603c0ef6b09df5f165fa2f5250c4256b0014
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-03-25 22:55:18 -0300

    refactor(InmuebleServiceImpl): se agregaron metodos para manejar logica de validacion

--- backend/src/main/java/idforideas/bonpland/service/impl/InmuebleServiceImpl.java
+++ backend/src/main/java/idforideas/bonpland/service/impl/InmuebleServiceImpl.java
@@ -14,10 +14,10 @@ import idforideas.bonpland.service.InmuebleService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -28,23 +28,19 @@ import java.util.Optional;
 public class InmuebleServiceImpl implements InmuebleService {
     private final InmuebleRepository inmuebleRepository;
     private final UsuarioRepository usuarioRepository;
-
     private final InmuebleMapper mapper;
     private CustomUserDetailService customUserDetailService;
 
     @Override
     @Transactional
     public Inmueble guardarInmueble(InmuebleDTO dto) {
-        String correo = SecurityContextHolder.getContext().getAuthentication().getName();
-        Usuario usuario = usuarioRepository.findByCorreo(correo)
-                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+        Usuario usuario = obtenerUsuarioAutenticado();
+        validarDtoNulo(dto);
 
-        if (dto == null) {
-            throw new IllegalArgumentException("Inmueble no puede ser nulo");
-        }
         Inmueble inmueble = mapper.map(dto);
         inmueble.setId(null);
         inmueble.setUsuario(usuario);
+
         return inmuebleRepository.save(inmueble);
     }
 
@@ -56,51 +52,61 @@ public class InmuebleServiceImpl implements InmuebleService {
     @Override
     public Inmueble buscarInmueblePorId(Long id) {
         Optional<Inmueble> inmueble = inmuebleRepository.findById(id);
-        return inmueble.orElseThrow(()-> new InmuebleNoEncontradoException("Inmueble no encontrado"));
+
+        return inmueble.orElseThrow(() -> new InmuebleNoEncontradoException("Inmueble no encontrado"));
     }
 
     @Override
     @Transactional
     public Inmueble actualizarInmueble(InmuebleDTO dto) {
-        String correo = SecurityContextHolder.getContext().getAuthentication().getName();
-        Usuario usuario = usuarioRepository.findByCorreo(correo)
-                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
-
-        if (dto == null) {
-            throw new IllegalArgumentException("Inmueble no puede ser nulo");
-        }
-
-        if (dto.id() == null) {
-            throw new IllegalArgumentException("El id no puede ser nulo para realizar esta acción");
-        }
+        Usuario usuario = obtenerUsuarioAutenticado();
+        validarDtoNulo(dto);
+        validarIdNulo(dto.id());
 
         Inmueble inmuebleEncontrado = inmuebleRepository.findById(dto.id())
                 .orElseThrow(() -> new InmuebleNoEncontradoException("Inmueble no encontrado"));
         inmuebleEncontrado = mapper.map(dto);
         inmuebleEncontrado.setUsuario(usuario);
+
         return inmuebleRepository.save(inmuebleEncontrado);
     }
 
     @Override
     @Transactional
     public void eliminarInmueble(Long id) {
-        String correo = SecurityContextHolder.getContext().getAuthentication().getName();
-        Usuario usuario = usuarioRepository.findByCorreo(correo)
-                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
-
-
-
-        if (id == null) {
-            throw new IllegalArgumentException("El id no puede ser nulo para realizar esta acción");
-        }
+        Usuario usuario = obtenerUsuarioAutenticado();
+        validarIdNulo(id);
 
         Inmueble inmueble = inmuebleRepository.findById(id)
                 .orElseThrow(() -> new InmuebleNoEncontradoException("Inmueble no encontrado"));
 
-        if (usuario.getId() != inmueble.getUsuario().getId()) {
-            throw new UsuarioSinPermisoException("No tienes permiso para realizar esta acción");
-        }
+        verificarDuenioDeInmueble(usuario, inmueble);
         inmuebleRepository.deleteById(id);
     }
 
+
+
+    private Usuario obtenerUsuarioAutenticado() {
+        String correo = SecurityContextHolder.getContext().getAuthentication().getName();
+        return usuarioRepository.findByCorreo(correo)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+    }
+
+    private void verificarDuenioDeInmueble(Usuario usuario, Inmueble inmueble) {
+        if (!Objects.equals(usuario.getId(), inmueble.getUsuario().getId())) {
+            throw new UsuarioSinPermisoException("No tienes permiso para realizar esta acción");
+        }
+    }
+
+    private void validarIdNulo(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("El id no puede ser nulo para realizar esta acción");
+        }
+    }
+
+    private void validarDtoNulo(InmuebleDTO dto) {
+        if (dto == null) {
+            throw new IllegalArgumentException("Inmueble no puede ser nulo");
+        }
+    }
 }
