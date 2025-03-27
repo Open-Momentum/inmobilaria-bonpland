fbd79cbd609c2b15a2ff757e46ba17bf65300223
FigueroaMauro <figueroamauro.dev@gmail.com>
2025-03-26 23:41:04 -0300

    refactor:(GlobalExceptionHandler): se unificaron los metodos segun su codigo de respuesta y se agregó el InmuebleNoEncontradoException al handler

--- backend/src/main/java/idforideas/bonpland/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/idforideas/bonpland/handler/GlobalExceptionHandler.java
@@ -21,43 +21,21 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UsuarioNoEncontradoException.class)
-    public ResponseEntity<Map<String, Object>> handleUsuarioNoEncontradoException(UsuarioNoEncontradoException ex) {
+    @ExceptionHandler({UsuarioNoEncontradoException.class,
+            RolNoEncontradoException.class,
+            InmuebleNoEncontradoException.class})
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(Exception ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
-    @ExceptionHandler(RolNoEncontradoException.class)
-    public ResponseEntity<Map<String, Object>> handleRolNoEncontradoException(RolNoEncontradoException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("error", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-    }
 
-    @ExceptionHandler(CorreoExistenteException.class)
-    public ResponseEntity<Map<String, Object>> handleCorreoExistente(CorreoExistenteException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("error", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    @ExceptionHandler(IdInexistenteException.class)
-    public ResponseEntity<Map<String, Object>> handleIdInexistenteException(IdInexistenteException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("error", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    @ExceptionHandler(CredencialesIncorrectasException.class)
-    public ResponseEntity<Map<String, Object>> handleCredencialesIncorrectasException(CredencialesIncorrectasException ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("error", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
-    @ExceptionHandler(CambioCorreoException.class)
-    public ResponseEntity<Map<String, Object>> handleCambioCorreoException(CambioCorreoException ex) {
+    @ExceptionHandler({CorreoExistenteException.class,
+            IdInexistenteException.class,
+            CredencialesIncorrectasException.class,
+            CambioCorreoException.class})
+    public ResponseEntity<Map<String, Object>> handleBadRequestException(Exception ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
@@ -65,7 +43,7 @@ public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(JwtException.class)
-    public ResponseEntity<Map<String, Object>> handleJwtException(JwtException ex) {
+    public ResponseEntity<Map<String, Object>> handleUnauthorizedException(Exception ex) {
         Map<String, Object> response = new HashMap<>();
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
@@ -75,8 +53,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult().getFieldErrors().stream()
-                                      .map(FieldError::getDefaultMessage)
-                                      .collect(Collectors.toList());
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
 
         Map<String, Object> response = new HashMap<>();
         response.put("error", errors.size() == 1 ? errors.get(0) : errors);
